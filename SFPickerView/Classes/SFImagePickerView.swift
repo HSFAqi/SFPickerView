807882bda5fc9a7c22e9ef5167225b60f48ffc0c
hsf <hsf_ios@sina.com>
2020-01-09 21:24:40 +0800

    修复StringPickerView ImagePickerView

--- SFPickerView/Classes/SFImagePickerView.swift
+++ SFPickerView/Classes/SFImagePickerView.swift
@@ -10,54 +10,53 @@ import UIKit
 
 class SFImagePickerView: SFBasePickerView {
 
-//    // MARK: - 单列
-//    /// 【Image】单列，类方法（单列时推荐使用）
-//    /// - Parameters:
-//    ///   - title: 标题
-//    ///   - appearance: 自定义外观
-//    ///   - dataSource: 数据源
-//    ///   - defaultIndex: 默认选中项
-//    ///   - isCallbackWhenSelecting: 选择时是否自动回调
-//    ///   - callback: 回调
-//    @discardableResult
-//    public final class func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, dataSource: SFPickerSingleData, defaultIndex: Int = 0, config: SFConfig?, callback: @escaping ((Int, String) -> Void)) -> SFBasePickerView{
-//        return SFBasePickerView.showPickerWithTitle(title, style: .imageView(appearance: appearance), dataSource: dataSource, defaultIndex: defaultIndex, config: config, callback: callback)
-//    }
-//    /// 【Image】单列，对象方法（单列时推荐使用）
-//    /// - Parameters:
-//    ///   - title: 标题
-//    ///   - appearance: 自定义外观
-//    ///   - dataSource: 数据源
-//    ///   - defaultIndex: 默认选中项
-//    ///   - config: 配置
-//    ///   - callback: 回调
-//    public final func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, dataSource: SFPickerSingleData, defaultIndex: Int = 0, config: SFConfig?, callback: @escaping ((Int, String) -> Void)) {
-//        self.showPickerWithTitle(title, style: .imageView(appearance: appearance), dataSource: dataSource, defaultIndex: defaultIndex, config: config, callback: callback)
-//    }
-//    
-//    // MARK: - 单列+多列+联动
-//    /// 【Image】单列+多列+联动，类方法
-//    /// - Parameters:
-//    ///   - title: 标题
-//    ///   - appearance: 自定义外观
-//    ///   - dataSource: 数据源
-//    ///   - defaultIndex: 默认选中项
-//    ///   - config: 配置
-//    ///   - callback: 回调
-//    @discardableResult
-//    public final class func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, mode: SFPickerDataMode, defaultIndexs: [Int]?, config: SFConfig?, callback: @escaping (([Int], [String]) -> Void)) -> SFBasePickerView{
-//        return SFBasePickerView.showPickerWithTitle(title, style: .imageView(appearance: appearance), mode: mode, defaultIndexs: defaultIndexs, config: config, callback: callback)
-//    }
-//    /// 【Image】单列+多列+联动，对象方法
-//    /// - Parameters:
-//    ///   - title: 标题
-//    ///   - appearance: 自定义外观
-//    ///   - dataSource: 数据源
-//    ///   - defaultIndex: 默认选中项
-//    ///   - config: 配置
-//    ///   - callback: 回调
-//    public final func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, mode: SFPickerDataMode, defaultIndexs: [Int]?, config: SFConfig?, callback: @escaping (([Int], [String]) -> Void)) {
-//        self.showPickerWithTitle(title, style: .imageView(appearance: appearance), mode: mode, defaultIndexs: defaultIndexs, config: config, callback: callback)
-//    }
-
+    // MARK: - 单列
+    /// 【Image】单列，类方法（单列时推荐使用）
+    /// - Parameters:
+    ///   - title: 标题
+    ///   - appearance: 自定义外观
+    ///   - dataSource: 数据源
+    ///   - defaultIndex: 默认选中项
+    ///   - isCallbackWhenSelecting: 选择时是否自动回调
+    ///   - callback: 回调
+    @discardableResult
+    public final class func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, dataSource: SFPickerSingleData, defaultIndex: Int = 0, config: SFConfig?, callback: @escaping ((Int, UIImage) -> Void)) -> SFBasePickerView{
+        return SFBasePickerView.showPickerWithTitle(title, style: .imageView(appearance: appearance), dataSource: dataSource, defaultIndex: defaultIndex, config: config, callback: callback as! ((Int, SFPickerDataProtocol?) -> Void))
+    }
+    /// 【Image】单列，对象方法（单列时推荐使用）
+    /// - Parameters:
+    ///   - title: 标题
+    ///   - appearance: 自定义外观
+    ///   - dataSource: 数据源
+    ///   - defaultIndex: 默认选中项
+    ///   - config: 配置
+    ///   - callback: 回调
+    public final func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, dataSource: SFPickerSingleData, defaultIndex: Int = 0, config: SFConfig?, callback: @escaping ((Int, UIImage) -> Void)) {
+        self.showPickerWithTitle(title, style: .imageView(appearance: appearance), dataSource: dataSource, defaultIndex: defaultIndex, config: config, callback: callback as! ((Int, SFPickerDataProtocol?) -> Void))
+    }
+    
+    // MARK: - 单列+多列+联动
+    /// 【Image】单列+多列+联动，类方法
+    /// - Parameters:
+    ///   - title: 标题
+    ///   - appearance: 自定义外观
+    ///   - dataSource: 数据源
+    ///   - defaultIndex: 默认选中项
+    ///   - config: 配置
+    ///   - callback: 回调
+    @discardableResult
+    public final class func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, mode: SFPickerDataMode, defaultIndexs: [Int]?, config: SFConfig?, callback: @escaping (([Int], [UIImage]) -> Void)) -> SFBasePickerView{
+        return SFBasePickerView.showPickerWithTitle(title, style: .imageView(appearance: appearance), mode: mode, defaultIndexs: defaultIndexs, config: config, callback: callback as! (([Int], [SFPickerDataProtocol?]) -> Void))
+    }
+    /// 【Image】单列+多列+联动，对象方法
+    /// - Parameters:
+    ///   - title: 标题
+    ///   - appearance: 自定义外观
+    ///   - dataSource: 数据源
+    ///   - defaultIndex: 默认选中项
+    ///   - config: 配置
+    ///   - callback: 回调
+    public final func showPickerWithTitle(_ title: String?, appearance: SFPickerImageViewAppearance?, mode: SFPickerDataMode, defaultIndexs: [Int]?, config: SFConfig?, callback: @escaping (([Int], [UIImage]) -> Void)) {
+        self.showPickerWithTitle(title, style: .imageView(appearance: appearance), mode: mode, defaultIndexs: defaultIndexs, config: config, callback: callback as! (([Int], [SFPickerDataProtocol?]) -> Void))
+    }
 }
