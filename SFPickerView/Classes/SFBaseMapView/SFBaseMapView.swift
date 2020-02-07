9aa6a5bb365f19c631e30a194bd76ba1fe2e73d1
hsf <hsf_ios@sina.com>
2020-02-07 16:30:46 +0800

    【feature】准备添加SFBaseMapView

---
+++ SFPickerView/Classes/SFBaseMapView/SFBaseMapView.swift
@@ -0,0 +1,17 @@
+//
+//  SFBaseTableView.swift
+//  SFPickerView_Example
+//
+//  Created by 黄山锋 on 2020/2/3.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+public class SFBaseMapView: SFBaseView {
+
+    
+}
+
+
+
